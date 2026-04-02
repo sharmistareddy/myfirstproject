e7b50ee340008d8f5ca5f661c7dfbd714cc05eb0
sharmistareddy <107638847+sharmistareddy@users.noreply.github.com>
2026-04-02 17:54:41 +0530

    Added first Python program

---
+++ app.py
@@ -0,0 +1,7 @@
+name=input("Enter your name: ")
+print(f"Hello {name}, welcome to my first Python GitHub project!")
+
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+
+print("Sum is:", a+b)
